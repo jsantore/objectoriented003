74d56fd6f8803da4b97acf72ae5f46b23df7d5cd
jsantore <jsantore@bridgew.edu>
2022-11-10 11:43:26 -0500

    start using sprites

--- Comp151Window.py
+++ Comp151Window.py
@@ -4,11 +4,13 @@ class Comp151Window(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height, "Demo Window")
         self.player =None
-        self.targets = []
+        self.targets = arcade.SpriteList()
         self.score = 0
 
     def setup(self):
-        pass
+        self.player = arcade.Sprite("f1-ship1-6.png")
+        rock = arcade.Sprite(":resources:images/space_shooter/meteorGrey_med1.png")
+        self.targets.append(rock)
 
     def on_update(self, delta_time):
         pass
