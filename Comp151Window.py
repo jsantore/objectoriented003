d68b1f6fc2f29d4e75deb9d55302ee2b157251b7
jsantore <jsantore@bridgew.edu>
2022-11-10 11:57:36 -0500

    moved two sprites to be visible

--- Comp151Window.py
+++ Comp151Window.py
@@ -11,11 +11,16 @@ class Comp151Window(arcade.Window):
         self.player = arcade.Sprite("f1-ship1-6.png")
         rock = arcade.Sprite(":resources:images/space_shooter/meteorGrey_med1.png")
         self.targets.append(rock)
+        rock.center_y = 500
+        rock.center_x = 700
+        self.player.center_x = 200
+        self.player.center_y = 500
 
     def on_update(self, delta_time):
         pass
 
     def on_draw(self):
         arcade.start_render()
-        #do drawing here
+        self.player.draw()
+        self.targets.draw()
         arcade.finish_render()
